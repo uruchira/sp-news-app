cacc7a428773d9b32b9c0cb1cfaf7f3357736b39
uruchira <uruchira@gmail.com>
2022-04-16 10:18:48 +0530

    New search box added

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,40 +1,59 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-import "../../styles.css";
+import useClickOutside from "../../hooks/useClickOutside";
 
-const SearchBox = () => {
+import "./styles.css";
+
+function SearchBox() {
+  const clickRef = useRef();
   const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function closeTextbox() {
+    setIsOpen(false);
+  }
 
   function handleOnClick() {
-    setIsOpen(true);
+    setIsOpen(!isOpen);
   }
+
+  function handleOnChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  useClickOutside(clickRef, closeTextbox);
 
   function handleOnSubmit(event) {
     event.preventDefault();
-    console.log("Form Submitted...");
+    if (!searchTerm) {
+      closeTextbox();
+    } else {
+      console.log(searchTerm + " is submitted");
+    }
   }
 
   return (
-    <>
+    <div className="sb-search-wrapper">
       <div
-        id="sb-search"
+        ref={clickRef}
         className={isOpen ? "sb-search sb-search-open" : "sb-search"}
       >
-        <form onSubmit={handleOnSubmit}>
-          <input
-            className="sb-search-input"
-            placeholder="Enter your search term..."
-            type="search"
-            value=""
-            name="search"
-            id="search"
-          />
-          <input className="sb-search-submit" type="button" />
-          <span className="sb-icon-search" onClick={handleOnClick}></span>
-        </form>
+        <input
+          type="search"
+          className="sb-search-input"
+          placeholder="Enter your search term..."
+          value={searchTerm}
+          onChange={handleOnChange}
+        />
+        <input
+          className="sb-search-submit"
+          type="button"
+          onClick={handleOnSubmit}
+        />
+        <span className="sb-icon-search" onClick={handleOnClick}></span>
       </div>
-    </>
+    </div>
   );
-};
+}
 
 export default SearchBox;
